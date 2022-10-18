48849067495b5b229fa6595545d548863066ed51
Adnan Bharmal <adnanbharmal153@gmail.com>
2022-10-18 16:27:30 +0530

    initial commit

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,38 +4,43 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = (song) => {
-  const activeSong="Test";
+const SongCard = ({song, i, activeSong, isPlaying, data}) => {
+  const dispatch = useDispatch();
   const handlePauseClick = () => {
+    dispatch(playPause(false))
   }
   const handlePlayClick = () => {
+    dispatch( setActiveSong ({ song, data, i }));
+    dispatch(playPause(true))
   }
   return (
-  <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-    <div className="relative w-full h-56 group">
-      <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title ===song.title ? 'hidden' : 'flex bg-black bg-opacity-70'} `}>
-      <PlayPause 
-      song={song}
-      handlePause={handlePauseClick}
-      handlePlay={handlePlayClick}
-      />
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group">
+        <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'hidden' : 'flex bg-black bg-opacity-70'} `}>
+          <PlayPause
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+            activeSong={activeSong}
+            isPlaying={isPlaying}
+          />
+        </div>
+        <img alt="song_img" src={song.images?.coverart} />
       </div>
-      <img alt="song_img" src={song.song.images?.coverart} />
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/songs/${song?.key}`}>
+            {song.title}
+          </Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : '/top-artists'}>
+            {song.subtitle}
+          </Link>
+        </p>
+      </div>
     </div>
-    <div className="mt-4 flex flex-col">
-      <p className="font-semibold text-lg text-white truncate">   
-        <Link to={`/songs/${song.song?.key}`}>
-        {song.song.title}
-        </Link>
-      </p>
-      <p className="text-sm truncate text-gray-300 mt-1">
-        <Link to={song.song.artists ? `/artists/${song.song?.artists[0]?.adamid}` : '/top-artists'}>
-        {song.song.subtitle}
-        </Link>
-      </p>
-    </div>
-  </div>
-)
+  )
 };
 
 export default SongCard;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,36 +1,43 @@
+import React from "react";
 import { Error, SongCard, Loader } from "../components";
 import { genres } from "../assets/constants";
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { useGetSongsByGenreQuery } from "../redux/services/shazamCore";
+import { useDispatch, useSelector } from "react-redux";
+import { selectGenreListId } from "../redux/features/playerSlice";
 const Discover = () => {
-    const { data, isFetching, error } = useGetTopChartsQuery();
+    const dispatch = useDispatch();
+    const { genreListId } = useSelector((state) => state.player);
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP');
+    const genreTitle = genres.find(({value})=>value===genreListId)?.title;
     // console.log(genres)
-    console.log(data);
-    const genreTitle = 'POP';
+    // console.log(data);
     if (isFetching) return <Loader title="Loading Songs..." />;
     if (error) return <Error />;
     return (
         <div className="flex flex-col">
-            <div className="w-full flex justify-between items-center flex-col mt-4 mb-10">
-                <h2 className="font-bold text-3xl text-white text-left">
-                    Discover {genreTitle}
-                </h2>
+            <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+                <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
                 <select
-                    onChange={() => { }}
-                    value=""
+                    onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+                    value={genreListId || 'POP'}
                     className="bg-black text-gray-300 p-3 text-sm-rounded-lg outline-none sm:mt-0 mt-5"
                 >
-                    {genres.map(genre => <option>{genre.title}</option>)}
+                    {genres.map(genre => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
                 </select>
-                <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-                    {data?.map((song, i) => (
-                        <SongCard
-                            key={song.key}
-                            song={song}
-                            i={i}
-                        />
-                    ))}
+            </div>
+            <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+                {data?.map((song, i) => (
+                    <SongCard
+                        key={song.key}
+                        song={song}
+                        i={i}
+                        activeSong={activeSong}
+                        isPlaying={isPlaying}
+                        data={data}
+                    />
+                ))}
 
-                </div>
             </div>
         </div>
     )
